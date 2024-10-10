51dfd305ff9beee09dff2f2cd95ce3a5cff3ffee
atfdeenk <atfdeenk@gmail.com>
2024-10-10 11:59:25 +0700

    revision

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 // ini file js
-function celsiusFahrenheit(){
-  let input = document.getElementById("celsius").value;
-  let hasil = input * 9/5 + 32;
-  document.getElementById("fahrenheit").value = hasil;
-  document.getElementById("metode").innerHTML = document.getElementById("metode").innerHTML.replace("S", input)
-  document.getElementById("metode").innerHTML = document.getElementById("metode").innerHTML.replace("S", hasil)
+function celsiusFahrenheit() {
+  let input = document.getElementById('celsius').value;
+  let hasil = (input * 9) / 5 + 32;
+  document.getElementById('fahrenheit').value = hasil;
+  document.getElementById('metode').innerHTML = document
+    .getElementById('metode')
+    .innerHTML.replace('S', input);
+  document.getElementById('metode').innerHTML = document
+    .getElementById('metode')
+    .innerHTML.replace('S', hasil);
 }
-function fahrenheitCelsius(){
-  let input = document.getElementById("fahrenheit").value;
-  let hasil = (input - 32) * 5/9;
-  document.getElementById("celsius").value = hasil;
-  document.getElementById("metode").innerHTML = document.getElementById("metode").innerHTML.replace("S", input)
-  document.getElementById("metode").innerHTML = document.getElementById("metode").innerHTML.replace("S", hasil)
+function fahrenheitCelsius() {
+  let input = document.getElementById('fahrenheit').value;
+  let hasil = ((input - 32) * 5) / 9;
+  document.getElementById('celsius').value = hasil;
+  document.getElementById('metode').innerHTML = document
+    .getElementById('metode')
+    .innerHTML.replace('S', input);
+  document.getElementById('metode').innerHTML = document
+    .getElementById('metode')
+    .innerHTML.replace('S', hasil);
 }
